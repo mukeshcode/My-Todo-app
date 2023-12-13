14b087fdf4b70aa20652c0d4a166e3beb02392b1
Jitendra Kumar <contactjittu@gmail.com>
2023-12-13 23:11:01 +0530

    server added

---
+++ server/app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3003;
+const mongoURL = process.env.MONGO_URL;
+
+app.set('port', port);
+app.use(express.json());
+
+mongoose.connect(mongoURL);
+
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    completed: { type: Boolean, default: false },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+app.use((req, res, next) => {
+    console.log('Method:',req.method, 'URL:', req.url);
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+// Create a new todo
+app.post('/todos', async (req, res) => {
+    try {
+        const newTodo = await Todo.create(req.body);
+        res.status(201).json(newTodo);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Error creating todo' });
+    }
+});
+
+// Get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching todos' });
+    }
+});
+
+// Update a todo
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const updatedTodo = await Todo.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.json(updatedTodo);
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating todo' });
+    }
+});
+
+// Delete a todo
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        await Todo.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Todo deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error deleting todo' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App is listing on port: ${port}`);
+})
